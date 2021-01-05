543f396edf7466210413f6f910f99fe47e7a3e88
laxman159 <56950807+laxman159@users.noreply.github.com>
2021-01-05 12:31:08 +0530

    Updates

---
+++ precache-manifest.8b654d07da42869cfe732ce5224bfcbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82e17768caeae5c065b5c349cfc1390",
+    "url": "/PIC_POST/index.html"
+  },
+  {
+    "revision": "77dd4bdd237b93484e60",
+    "url": "/PIC_POST/static/css/main.69b19522.chunk.css"
+  },
+  {
+    "revision": "df6ff79ca7b77cfb4cd3",
+    "url": "/PIC_POST/static/js/2.be1a421c.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/PIC_POST/static/js/2.be1a421c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77dd4bdd237b93484e60",
+    "url": "/PIC_POST/static/js/main.6c7cfb8b.chunk.js"
+  },
+  {
+    "revision": "83f9477bdb887cc5eeac",
+    "url": "/PIC_POST/static/js/runtime-main.6b5e959b.js"
+  }
+]);
